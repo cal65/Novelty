7d03611adf4e5ae78abdbaeace705d4237cd02ed
Cal Lee <cal.christopheralee@gmail.com>
2022-05-07 12:34:23 -0700

    feat: adding generic function that can be used to update tables in database
    Previously everything was hardcoded for authors and books.
    I want to populate a new region nationality mapping table

--- scripts/make_authors_db.py
+++ scripts/make_authors_db.py
@@ -4,6 +4,29 @@ from sqlalchemy import create_engine, insert
 import pandas as pd
 from datetime import datetime
 
+def populate_table_from_csv(csv_file_path, table, database="goodreads"):
+    """ populate table in the PostgreSQL database"""
+    try:
+        connection = psycopg2.connect(
+            user="cal65",
+            password=os.environ["cal65_pass"],
+            host="127.0.0.1",
+            port="5432",
+            database=database,
+        )
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    engine = create_engine(
+        "postgresql+psycopg2://cal65:"
+        + os.environ["cal65_pass"]
+        + "@127.0.0.1/goodreads"
+    )
+    df = pd.read_csv(csv_file_path)
+    df.to_sql(table, engine, if_exists="replace", index=False)
+    connection.close()
+
 
 def start():
     """ populate authors table in the PostgreSQL database"""
