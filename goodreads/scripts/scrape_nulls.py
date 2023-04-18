426326c88f4a2afa8b06f89e6391265c528f5e11
Cal Lee <cal.christopheralee@gmail.com>
2023-04-18 13:51:29 -0700

    feat: helper script for scraping
    To be imported in shell_plus

---
+++ goodreads/scripts/scrape_nulls.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from goodreads.models import *
+from goodreads.scripts.append_to_export import append_scraping
+
+
+def scrape_null(book_ids, n):
+    for b in book_ids[:n]:
+        b = append_scraping(b.book_id, wait=25)
+        b.save()
+        print(b.added_by)
+
+def get_missing_books():
+    export_all = ExportData.objects.all()
+    books_all = Books.objects.all()
+    books_ids = [b.book_id for b in books_all]
+    missing_ids = [e.book_id for e in export_all if e.book_id not in books_ids]
+    return missing_ids
+
+def get_nulls():
+    books_null = Books.objects.filter(added_by__isnull=True)
+    print(len(books_null))
+    null_ids = [b.book_id for b in books_null]
+    return null_ids
