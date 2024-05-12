3b5f209d093abc42c62763dfee21e44136e8f512
Cal Lee <cal.christopheralee@gmail.com>
2024-05-12 13:29:08 -0700

    fix: issue in scrape
    Missing cookies and headers in user list function
    Missing page.content in article function

--- goodreads/scripts/scrape_article.py
+++ goodreads/scripts/scrape_article.py
@@ -1,9 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-import numpy as np
-import random
-import time
 import sys
 
 cookies = {
@@ -52,7 +48,7 @@ headers = {
 
 def ids_from_article(url):
     page = requests.get(url, cookies=cookies, headers=headers, timeout=5)
-    soup = BeautifulSoup(page, "html.parser")
+    soup = BeautifulSoup(page.content, "html.parser")
     links_raw = soup.findAll('i')
     a_refs = [link.find('a') for link in links_raw]
     a_refs = [a for a in a_refs if a is not None]
@@ -62,7 +58,7 @@ def ids_from_article(url):
 
 
 def ids_from_user_list(url):
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    soup = BeautifulSoup(requests.get(url, cookies=cookies, headers=headers).content, "html.parser")
     titles = soup.findAll('td', {'class': 'field title'})
     links_raw = [t.find('a').attrs['href'] for t in titles]
     book_ids = [int(link.split('/')[-1].split('-')[0].split('.')[0]) for link in links_raw]
