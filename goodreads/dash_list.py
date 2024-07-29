f8afddf08cd143cc75eeee258e594a3548ed6489
Cal Lee <cal.christopheralee@gmail.com>
2024-07-29 16:21:33 -0700

    feat: adding to navbar and adding bold
    Got the bold numbers

--- goodreads/dash_list.py
+++ goodreads/dash_list.py
@@ -10,7 +10,7 @@ external_stylesheets = [
 app = DjangoDash('checklist',  external_stylesheets=external_stylesheets)
 
 df = pd.read_csv('artifacts/nyt_merged.csv')
-df['title_author'] = df['rank'].astype(str) + '. ' + df['title'] + ' - ' + df['author']
+df['title_author'] = df['title'] + ' - ' + df['author']
 
 app.layout = html.Div(
             className='checklistContainer',
@@ -18,7 +18,8 @@ app.layout = html.Div(
                 dcc.Checklist(
                     id='checklist',
                     className='checklist',
-                    options=[{'label': i, 'value': i} for i in df['title_author']],
+                    options=[{'label': html.Span([html.B(str(100-i) + '. '), t]),
+                              'value': i} for i, t in enumerate(df['title_author'])],
                     value=[],
                     labelStyle={'display': 'block'}
                 ),
