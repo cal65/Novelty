361fbfb5cd34da11435f74a4594b9c312ff6e44f
DJ Lee <dennisjlee@gmail.com>
2025-05-05 18:54:37 -0400

    Add dotenv parsing in dev

--- manage.py
+++ manage.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
+from dotenv import load_dotenv
 import os
 import sys
 
 
 def main():
+    # take environment variables from .env file if it exists.
+    # Note that manage.py is not used in production, so this will not have an effect there.
+    load_dotenv()
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goodreads_history.settings")
     try:
         from django.core.management import execute_from_command_line
