60c809227475e562e389ecc9c37bf063aa6baa9c
DJ Lee <dennisjlee@gmail.com>
2025-05-12 16:08:54 -0400

    Configure django-tailwind
    This also includes deleting a few duplicate rules.
    
    In development mode, we now need to run this to get the tailwind CSS to
    regenerate automatically:
    
    `python manage.py tailwind start`
    
    Before deploying to production, we should run this to generate the
    minified production CSS and then check that into Git:
    
    `python manage.py tailwind build`
    
    Note that the `theme` directory was auto-generated by this command:
    
    `python manage.py tailwind init`

--- goodreads_history/urls.py
+++ goodreads_history/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('goodreads.urls')),
     path('users/', include('users.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
