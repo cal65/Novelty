7d915cfbf1b3c56f9e027c3f97edbe18d4b71798
DJ Lee <dennisjlee@gmail.com>
2025-05-16 10:21:47 -0400

    Move first-party files out of static/admin

--- scripts/update_galleries.sh
+++ scripts/update_galleries.sh
@@ -1,14 +1,14 @@
 for f in static/Graphs/cal65/goodreads*html
 do
-	cp "$f" goodreads/static/admin/img/goodreads
+	cp "$f" goodreads/static/novelty/goodreads
 done
 
 for f in static/Graphs/cal65/spotify*html
 do
-	cp "$f" goodreads/static/admin/img/spotify
+	cp "$f" goodreads/static/novelty/spotify
 done
 
 for f in static/Graphs/cal65/netflix*html
 do
-	cp "$f" goodreads/static/admin/img/netflix
+	cp "$f" goodreads/static/novelty/netflix
 done
