02d52588ea75198690fe1a9b06a1003c6762f88d
DJ Lee <dennisjlee@gmail.com>
2025-05-20 16:23:30 -0400

    Install and set up django-vite with React

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import {join, resolve} from 'node:path';
+import {defineConfig, loadEnv} from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    const INPUT_DIR = './goodreads/vite_assets';
+    const OUTPUT_DIR = join(INPUT_DIR, '/dist');
+
+    return {
+        plugins: [react()],
+        root: resolve(INPUT_DIR),
+
+        base: "/static/",
+        server: {
+            host: env.DJANGO_VITE_DEV_SERVER_HOST,
+            port: parseInt(env.DJANGO_VITE_DEV_SERVER_PORT, 10),
+        },
+        build: {
+            manifest: "manifest.json",
+            emptyOutDir: true,
+            outDir: resolve(OUTPUT_DIR),
+            rollupOptions: {
+                input: {
+                   main: join(INPUT_DIR, '/apps/main.tsx'),
+                }
+            }
+
+        }
+    };
+})
